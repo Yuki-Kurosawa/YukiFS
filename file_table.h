2396caaf17bc29a99171c15570de86f52fa6be95
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-03-14 15:42:06 +0800

    add superblock struct

--- file_table.h
+++ file_table.h
@@ -12,6 +12,24 @@
 #define FS_FREE_INODES 0
 // end define superblock info
 
+struct superblock_info {
+    uint8_t block_size;
+    uint8_t block_count;
+    uint8_t block_free;
+    uint8_t block_available;
+    uint8_t total_inodes;
+    uint8_t free_inodes;
+};
+
+static const struct superblock_info fs_superblock = {
+    .block_size = FS_BLOCK_SIZE,
+    .block_count = FS_BLOCK_COUNT,
+    .block_free = FS_BLOCK_FREE,
+    .block_available = FS_BLOCK_AVAILABLE,
+    .total_inodes = FS_TOTAL_INODES,
+    .free_inodes = FS_FREE_INODES
+};
+
 struct file_table
 {
     char name[FS_MAX_LEN]; //file name
