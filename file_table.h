bea5f9bf7979491da1049af5db55133e0efde567
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-03-14 15:40:01 +0800

    move FS* marcos to file_table

--- file_table.h
+++ file_table.h
@@ -3,6 +3,15 @@
 
 #define FS_MAX_LEN 8
 
+// define a tiny superblock info
+#define FS_BLOCK_SIZE 1 // dd bs value
+#define FS_BLOCK_COUNT 1 // dd count value
+#define FS_BLOCK_FREE 0
+#define FS_BLOCK_AVAILABLE 0
+#define FS_TOTAL_INODES 1
+#define FS_FREE_INODES 0
+// end define superblock info
+
 struct file_table
 {
     char name[FS_MAX_LEN]; //file name
--- internal.h
+++ internal.h
@@ -13,15 +13,7 @@
 
 #define FILE_DEFAULT_PERMISSION 0755
 
-// define a tiny superblock info
-#define FS_BLOCK_SIZE 1 // dd bs value
-#define FS_BLOCK_COUNT 1 // dd count value
-#define FS_BLOCK_FREE 0
-#define FS_BLOCK_AVAILABLE 0
-#define FS_TOTAL_INODES 1
-#define FS_FREE_INODES 0
 
-// end define superblock info
 
 struct yukifs_inode {
     int i_mode;
