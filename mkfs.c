d6876849e33309df3a4a72fe71439ec5060efd54
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-03-17 16:26:18 +0800

    Add ELF Header to image file

--- mkfs.c
+++ mkfs.c
@@ -54,7 +54,62 @@ void gen_hidden_data(unsigned char data[], uint32_t block_size)
 
 void gen_fs_padding_data(unsigned char padding[], int device_type)
 {
-    
+    if(S_ISBLK(device_type))
+    {
+        memset(padding,0x00,FS_PADDING_SIZE);
+    }
+    else
+    {
+        // DO SOME ELF THINGS HERE;
+        unsigned char ELF[FS_PADDING_SIZE]={0};
+
+        unsigned char elf_sign[]={0x7F, 0x45, 0x4C, 0x46}; // ELF IDENT EI_MAG0~EI_MAG3
+        memcpy(ELF, elf_sign,sizeof(elf_sign));
+
+        unsigned char elf_header[] ={
+            0x02,                    // Class: 64-bit
+            0x01,                    // Data: Little-endian
+            0x01,                    // Version: 1 (original)
+            0x03,                    // OS/ABI: Linux
+            0x00,                    // ABI Version: 0
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Padding (7 bytes)
+            0x02, 0x00,              // Type: Executable (ET_EXEC)
+            0x3e, 0x00,              // Machine: x86-64 (EM_X86_64)
+            0x01, 0x00, 0x00, 0x00,  // Version (ELF): 1
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Entry point address (PhyAddr)
+            0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Program header offset
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Section header offset
+            0x00, 0x00, 0x00, 0x00,  // Flags
+            0x40, 0x00,              // Size of this header
+            0x38, 0x00,              // Size of program header entries
+            0x01, 0x00,              // Number of program header entries
+            0x00, 0x00,              // Size of section header entries
+            0x00, 0x00,              // Number of section header entries
+            0x00, 0x00               // Section header string table index
+            };
+        memcpy(ELF + 4, elf_header,sizeof(elf_header));
+
+        unsigned char elf_program_header[] = {
+            0x01, 0x00, 0x00, 0x00,  // p_type: LOAD (PT_LOAD)
+            0x07, 0x00, 0x00, 0x00,  // p_flags: Read, Write, Execute
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // p_offset: Offset in file
+            0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // p_vaddr: Virtual address
+            0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // p_paddr: Physical address
+            0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // p_filesz: Size in file
+            0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // p_memsz: Size in memory
+            0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00   // p_align: Alignment (4096)
+        };
+        memcpy(ELF + 4 + sizeof(elf_header), elf_program_header,sizeof(elf_program_header));
+
+        unsigned char program_data[]={
+            0xb8, 0x3c, 0x00, 0x00, 0x00,  // mov eax, 60 (exit syscall number)
+            0xbf, 0x00, 0x00, 0x00, 0x00,  // mov edi, 0 (exit code)
+            0x0f, 0x05                     // syscall
+        };
+        memcpy(ELF + 4 + sizeof(elf_header)+sizeof(elf_program_header), program_data,sizeof(program_data));
+
+        memcpy(padding,ELF,FS_PADDING_SIZE);
+    }
 }
 
 int gen_fs_header(unsigned char *header, unsigned char padding[], unsigned char hidden_data[],
--- testmkfs.sh
+++ testmkfs.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 clear
+unset LANG
+unset LANGUAGE
 dd if=/dev/zero of=test.img bs=1KiB count=10
 make -f Makefile.mkfs
 ./mkfs.yukifs -v
 ./mkfs.yukifs -h
 # ./mkfs.yukifs /dev/memory/20480
 ./mkfs.yukifs -y test.img
+chmod a+x test.img
+file test.img
+./test.img
+objdump -x test.img
 make clean
 #rm -rf test.img
