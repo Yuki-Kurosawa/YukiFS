36008c6fb19c1ffe9685f9e19fe28a2ee3f04679
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-03-31 16:44:17 +0800

    update test script

--- test.sh
+++ test.sh
@@ -1,24 +1,32 @@
 #! /bin/bash
+unset LANG
+unset LANGUAGE
+
 clear
-cd src/ko
-make 
-modinfo yukifs.ko
-insmod yukifs.ko
+make all install > /dev/null 2>&1
+modinfo src/ko/yukifs.ko
+insmod src/ko/yukifs.ko
 mkdir fs
 cat /proc/filesystems | grep yuki
-mount -t yuki yuki fs
+
+dd if=/dev/zero of=test.img bs=1KiB count=100
+mkfs.yukifs -y test.img
+
+mount -t yuki -o loop test.img fs
 mount | grep yuki
 
+ls -alc . | grep fs
 ls -alc fs
 touch fs/test.txt
 cat fs/version.txt
 cd fs/test_folder
-cd ../..
+cd ../../
 file fs/version.txt
 
 df -kh fs
 umount fs
 rm -rvf fs
-rmmod yukifs.ko
-make clean
-cd ../../
+rmmod yukifs
+make remove clean > /dev/null 2>&1
+
+rm -rvf test.img
