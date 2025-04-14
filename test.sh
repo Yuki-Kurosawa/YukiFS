841fea49c2bb7146424a624d979dfacd408b7c8d
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-04-14 11:45:06 +0800

    update test script

--- test.sh
+++ test.sh
@@ -4,25 +4,25 @@ unset LANGUAGE
 
 clear
 make all install > /dev/null 2>&1
-modinfo src/ko/yukifs.ko
-insmod src/ko/yukifs.ko
-mkdir fs
-cat /proc/filesystems | grep yuki
+modinfo src/ko/yukifs.ko > /dev/null 2>&1
+insmod src/ko/yukifs.ko > /dev/null 2>&1
+mkdir fs > /dev/null 2>&1
+cat /proc/filesystems | grep yuki > /dev/null 2>&1
 
-dd if=/dev/zero of=test.img bs=1KiB count=45
-mkfs.yukifs -y test.img
+dd if=/dev/zero of=test.img bs=1KiB count=45 > /dev/null 2>&1
+mkfs.yukifs -y test.img > /dev/null 2>&1
 
-infofs.yukifs test.img
-infofs.yukifs -s test.img #> /dev/null 2>&1
+infofs.yukifs test.img > /dev/null 2>&1
+infofs.yukifs -s test.img > /dev/null 2>&1
 
 #mount -t yuki -o loop test.img fs
-mount test.img fs
-mount | grep yuki
+mount test.img fs > /dev/null 2>&1
+mount | grep yuki > /dev/null 2>&1
 
 
-file fs
-ls -alci . | grep fs
-ls -alci fs
+file fs  > /dev/null 2>&1
+ls -alci . | grep fs > /dev/null 2>&1
+ls -alci fs > /dev/null 2>&1
 touch fs/test.txt
 infofs.yukifs -s test.img
 echo 123 >> fs/test.txt
@@ -30,19 +30,16 @@ echo 456 >> fs/test.txt
 echo "O TESTED"
 cat fs/test.txt
 echo "I TESTED"
-ls -alci fs
-#cat fs/version.txt
-#cd fs/test_folder
-#cd ../../
-#file fs/version.txt
+ls -alci fs > /dev/null 2>&1
 
-df -kh fs
-rm -rvf fs/test.txt
-umount fs
-rm -rvf fs
-rmmod yukifs
+
+df -kh fs > /dev/null 2>&1
+rm -rvf fs/test.txt > /dev/null 2>&1
+umount fs > /dev/null 2>&1
+rm -rvf fs > /dev/null 2>&1
+rmmod yukifs > /dev/null 2>&1
 make remove clean > /dev/null 2>&1
 
-cp test.img disk.img
-rm -rvf test.img
+cp test.img disk.img > /dev/null 2>&1
+rm -rvf test.img > /dev/null 2>&1
 #dmesg
