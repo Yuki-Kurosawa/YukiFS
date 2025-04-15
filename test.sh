5243a6d0f03b805faf6b88f25314f07c53338644
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-04-15 14:45:25 +0800

    update scripts

--- test.sh
+++ test.sh
@@ -27,7 +27,7 @@ umount fs
 echo "----- Test Case 1 Begin -----"
 echo "Operation: echo 123 > test.txt (test.txt does not exist)"
 mount -t yuki -o loop test.img fs 
-touch fs/test.txt
+# touch fs/test.txt
 echo 123 > fs/test.txt
 echo "Expected: 31 32 33 0A 00 00 00 00 00 00 00 00"
 echo -n "Actual: "
@@ -37,43 +37,43 @@ ls -alci fs
 umount fs > /dev/null 2>&1
 echo "----- Test Case 1 End -----"
 
-echo "----- Test Case 2 Begin -----"
-echo "Operation: echo 456 >> test.txt (test.txt exists)"
-mount test.img fs > /dev/null 2>&1
-echo 456 >> fs/test.txt
-echo "Expected: 31 32 33 0A 34 35 36 0A 00 00 00 00"
-echo -n "Actual: "
-viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
-infofs.yukifs -s test.img
-ls -alci fs
-umount fs > /dev/null 2>&1
-echo "----- Test Case 2 End -----"
+# echo "----- Test Case 2 Begin -----"
+# echo "Operation: echo 456 >> test.txt (test.txt exists)"
+# mount test.img fs > /dev/null 2>&1
+# echo 456 >> fs/test.txt
+# echo "Expected: 31 32 33 0A 34 35 36 0A 00 00 00 00"
+# echo -n "Actual: "
+# viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
+# infofs.yukifs -s test.img
+# ls -alci fs
+# umount fs > /dev/null 2>&1
+# echo "----- Test Case 2 End -----"
 
-echo "----- Test Case 3 Begin -----"
-echo "Operation: echo 123 > test.txt (test.txt exists)"
-mount test.img fs > /dev/null 2>&1
-echo 123 > fs/test.txt
-echo "Expected: 31 32 33 0A 00 00 00 00 00 00 00 00"
-echo -n "Actual: "
-viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
-infofs.yukifs -s test.img
-ls -alci fs
-umount fs > /dev/null 2>&1
-echo "----- Test Case 3 End -----"
+# echo "----- Test Case 3 Begin -----"
+# echo "Operation: echo 123 > test.txt (test.txt exists)"
+# mount test.img fs > /dev/null 2>&1
+# echo 123 > fs/test.txt
+# echo "Expected: 31 32 33 0A 00 00 00 00 00 00 00 00"
+# echo -n "Actual: "
+# viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
+# infofs.yukifs -s test.img
+# ls -alci fs
+# umount fs > /dev/null 2>&1
+# echo "----- Test Case 3 End -----"
 
-echo "----- Test Case 4 Begin -----"
-echo "Operation: echo 456 >> test.txt (test.txt does not exists)"
-mkfs.yukifs -y test.img > /dev/null 2>&1
-mount test.img fs > /dev/null 2>&1
-touch fs/test.txt
-echo 456 >> fs/test.txt
-echo "Expected: 34 35 36 0A 00 00 00 00 00 00 00 00"
-echo -n "Actual: "
-viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
-infofs.yukifs -s test.img
-ls -alci fs
-umount fs > /dev/null 2>&1
-echo "----- Test Case 4 End -----"
+# echo "----- Test Case 4 Begin -----"
+# echo "Operation: echo 456 >> test.txt (test.txt does not exists)"
+# mkfs.yukifs -y test.img > /dev/null 2>&1
+# mount test.img fs > /dev/null 2>&1
+# touch fs/test.txt
+# echo 456 >> fs/test.txt
+# echo "Expected: 34 35 36 0A 00 00 00 00 00 00 00 00"
+# echo -n "Actual: "
+# viewfs.yukifs --if=test.img --block-num=1 --count=12 --format=hex
+# infofs.yukifs -s test.img
+# ls -alci fs
+# umount fs > /dev/null 2>&1
+# echo "----- Test Case 4 End -----"
 
 ls -alci fs > /dev/null 2>&1
 
